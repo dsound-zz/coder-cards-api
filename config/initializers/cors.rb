62bd5ea7ae17e53370a867273954a3bcb8dd0462
dsound-zz <demiansims@gmail.com>
2020-07-20 08:04:46 -0400

    fixes

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'https://codercards-app.herokuapp.com'
+    origins 'http://localhost:3000','http://localhost:5000', 'https://codercards-app.herokuapp.com'
 
     resource '*',
       headers: :any,
