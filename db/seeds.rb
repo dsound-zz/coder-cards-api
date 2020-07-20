c6b5b45e73da6947194fbf6e1e9058698ce47876
dsound-zz <demiansims@gmail.com>
2020-07-19 23:43:11 -0400

    one more seed fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.destroy_all
-Deck.destroy_all 
-Card.destroy_all 
+# User.destroy_all
+# Deck.destroy_all 
+# Card.destroy_all 
 
 # Users
 
